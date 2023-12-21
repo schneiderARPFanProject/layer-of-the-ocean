fe876fd252dd9968248bd8c981b2546ca1cff592
Punyisa Kraisang <punyisa.kra@gmail.com>
2023-12-21 23:57:22 +0700

    Add light boc when click image in gallery

--- src/app/galleries/galleries.component.ts
+++ src/app/galleries/galleries.component.ts
@@ -1,15 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router'
+import { animate, style, transition, trigger } from '@angular/animations';
 import { GalleriesMessage } from '../galleries/galleries-message';
 import { MessageService } from '../galleries/services/message.service';
 
 @Component({
   selector: 'app-galleries',
   templateUrl: './galleries.component.html',
-  styleUrl: './galleries.component.css'
+  styleUrl: './galleries.component.css',
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({transform: 'scale(0.5)'}),
+        animate('100ms', style({transform: 'scale(1)'}))
+      ]),
+      transition('visible => void', [
+        style({transform: 'scale(1)'}),
+        animate('100ms', style({transform: 'scale(0.5)'}))
+      ])
+    ])
+  ]
 })
 export class GalleriesComponent {
   galleriesMessages: GalleriesMessage[] = [];
+  showPreview: boolean = false;
+  lightBoxImageUrl: string = "";
 
   @Input() level?: string;
 
@@ -26,5 +41,15 @@ export class GalleriesComponent {
   getGalleriesMessages(): void {
     this.messageService.getMessages(this.level!).subscribe(messages => this.galleriesMessages = messages);
     console.log("get galleries of", this.level);
+  }
+
+  onPreviewImage(imgUrl: string): void {
+    this.showPreview = true;
+    this.lightBoxImageUrl = imgUrl;
+  }
+
+  onClosePreview(): void {
+    this.showPreview = false;
+    this.lightBoxImageUrl = "";
   }
 }

59c4d90f8e0503d7d6695423f4a15325168a8716
Punyisa Kraisang <punyisa.kra@gmail.com>
2024-01-16 20:13:29 +0700

    Fix inject() warning, Add link to credit

--- src/app/navigation.guard.ts
+++ src/app/navigation.guard.ts
@@ -2,10 +2,11 @@ import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
 
 export const navigationGuard: CanActivateFn = (route, state) => {
-  if (inject(Router).navigated) {
+  const router = inject(Router);
+  if (router.navigated) {
     return true
   } else {
-    inject(Router).navigate(['/']).then(() => {inject(Router).navigate(['/'])})
+    router.navigate(['/']).then(() => {router.navigate(['/'])})
     return false
   };
 };

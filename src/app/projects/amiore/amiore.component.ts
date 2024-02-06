1261b87236a523a5a66b85ff562e167c53150c68
Punyisa Kraisang <punyisa.kra@gmail.com>
2024-02-07 01:16:55 +0700

    Add galleries to cafe projects

--- src/app/projects/amiore/amiore.component.ts
+++ src/app/projects/amiore/amiore.component.ts
@@ -1,15 +1,27 @@
-
 import { Component} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BaseProject } from '../base-project.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
-selector: 'app-amiore',
-templateUrl: './amiore.component.html',
-styleUrl: './amiore.component.css'
+  selector: 'app-amiore',
+  templateUrl: './amiore.component.html',
+  styleUrl: './amiore.component.css',
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({opacity: 0}),
+        animate('300ms', style({opacity: 1}))
+      ]),
+      transition('visible => void', [
+        style({opacity: 1}),
+        animate('300ms', style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class AmioreComponent extends BaseProject {
-constructor(route: ActivatedRoute) {
+  constructor(route: ActivatedRoute) {
     super(route);
     this.projectName = 'amiore';
   }

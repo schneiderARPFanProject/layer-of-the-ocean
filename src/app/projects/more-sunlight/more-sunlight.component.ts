fd9ca216ea3f8e94e0a6b858d88d7b4ccf61edc0
Punyisa Kraisang <punyisa.kra@gmail.com>
2024-02-08 17:27:31 +0700

    Add tamboon project and project galleries

--- src/app/projects/more-sunlight/more-sunlight.component.ts
+++ src/app/projects/more-sunlight/more-sunlight.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BaseProject } from '../base-project.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-more-sunlight',
   templateUrl: './more-sunlight.component.html',
-  styleUrl: './more-sunlight.component.css'
+  styleUrl: './more-sunlight.component.css',
+  animations: [
+  trigger(
+    'animation', [
+      transition('void => visible', [
+        style({opacity: 0}),
+        animate('300ms', style({opacity: 1}))
+      ]),
+      transition('visible => void', [
+        style({opacity: 1}),
+        animate('300ms', style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class MoreSunlightComponent extends BaseProject{
   constructor(route: ActivatedRoute) {
